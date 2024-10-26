4d4bc9bd2c77ea8958b65f2625d294cf9249173e
jxxnixx <dkepffk10@gmail.com>
2024-10-26 16:41:35 +0900

    feat: CRUD practice

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Worlds!!';
+  }
+
+  cuteCute(abc: string): string {
+    return `cute is cuter than ${abc}`;
   }
 }
